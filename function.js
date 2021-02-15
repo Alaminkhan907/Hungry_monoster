9808206370052338a6d0d5842b2cf6720c60262f
mdkhaz <mdkhaz@ttu.ee>
2021-02-16 00:21:31 +0200

    photo-add

--- function.js
+++ function.js
@@ -5,14 +5,8 @@ function callApi(meal) {
     //console.log(url);
      fetch(url)
       .then((res) => res.json())
-      .then((data) => {data.meals.forEach(meal =>{
-        console.log(meal.strMeal);
-        const mealPhotoDiv =document.getElementById('meal-items');
-        mealPhotoDiv.innerHTML =`
-        <img src="${meal.strMealThumb}">
-        <h5>${meal.strMeal}</h5>
-        `
-        })
+      .then((data) => {
+        foodMenu(data)
       })
       .catch(error =>alert("Wrong Food Name"))
   }
@@ -20,5 +14,23 @@ function callApi(meal) {
     const inputFood = document.getElementById("input-food").value;
     console.log(inputFood);
     callApi(inputFood);
+  }
+  function foodMenu(data){
+    const foodName =document.getElementById("meal-items");
+    data.meals.forEach(meal =>{
+      console.log(meal.strMeal);
+      let mealItemsDiv = document.createElement('div');
+      console.log(mealItemsDiv);
+      mealItemsDiv.className = 'col-md-3 item-columns';
+      console.log(mealItemsDiv.className);
+      let cardHtml = `
+      <div class="card rounded-3 border-0">
+      <img src = "${meal.strMealThumb}" class="card-img-top" alt="...">
+      <div class="card-body">
+          <h5 class="text-center">${meal.strMeal}</h5>
+      </div>`;
+      mealItemsDiv.innerHTML = cardHtml;
+      foodName.appendChild(mealItemsDiv);
+      })
   }
     
